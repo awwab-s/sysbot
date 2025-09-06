debfdfd8d8ccd8e690ced1fb8eb983008ddbec66
awwab <awwabsamad123@gmail.com>
2025-09-06 06:43:36 +0500

    Fixed issue in config

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
 PREFIX = os.getenv("PREFIX")
-CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+CHANNEL_ID = int(os.getenv("CHANNEL_ID", "0"))
 CHAT_API_KEY = os.getenv("CHAT_API_KEY")
 CHAT_MODEL = os.getenv("CHAT_MODEL")
 
