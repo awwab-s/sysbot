a204d5b0ab356e156e7bd34915bdb122f3c96f2f
awwab <awwabsamad123@gmail.com>
2025-09-22 19:35:56 +0500

    Fixed port issue

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from threading import Thread
 
@@ -12,6 +13,7 @@ def health():
     return "OK", 200
 
 def run():
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=8080, debug=False, use_reloader=False)
 
 def keep_alive():
